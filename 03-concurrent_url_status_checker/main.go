4a99c3e04fbf8f4173306891991f1db19b2bff49
Harish Yadav <harish.y@greyorange.com>
2025-12-31 08:13:58 +0530

    Add retry feature to project 03

--- 03-concurrent_url_status_checker/main.go
+++ 03-concurrent_url_status_checker/main.go
@@ -13,6 +13,10 @@ import (
 	"time"
 )
 
+const (
+	intialBackoff = 200 * time.Millisecond
+)
+
 type URLStatus struct {
 	URL          string
 	StatusCode   int
@@ -23,10 +27,11 @@ func main() {
 	startTime := time.Now()
 	urlFileName := flag.String("file", "urls.txt", "File containing URLs (one per line)")
 	rateLimitFlag := flag.Bool("rate_lim", false, "Flag to enable rate limiting")
+	retries := flag.Int("retries", 0, "Number of retries")
 	flag.Parse()
 
 	urls := fetchURLs(*urlFileName)
-	urlsStatus := checkURLs(urls, *rateLimitFlag)
+	urlsStatus := checkURLs(urls, *rateLimitFlag, *retries)
 	printURLsStatus(urlsStatus)
 
 	totalTimeTaken := time.Since(startTime)
@@ -55,7 +60,7 @@ func fetchURLs(fileName string) (urls []string) {
 	return
 }
 
-func checkURLs(urls []string, rateLimitFlag bool) []URLStatus {
+func checkURLs(urls []string, rateLimitFlag bool, retries int) []URLStatus {
 	workerCount := 5
 
 	// create channel for sending jobs
@@ -76,7 +81,7 @@ func checkURLs(urls []string, rateLimitFlag bool) []URLStatus {
 
 	// create workers
 	for i := 0; i < workerCount; i++ {
-		go worker(jobsCh, resultsCh, &wg, rateLimitTicker, rateLimitFlag)
+		go worker(jobsCh, resultsCh, &wg, rateLimitTicker, rateLimitFlag, retries)
 	}
 
 	// send urls on jobCh and close the channel
@@ -137,8 +142,9 @@ func worker(
 	jobsCh <-chan string, 
 	resultsCh chan<- URLStatus, 
 	wg *sync.WaitGroup,
-	rateLimitTicker *time.Ticker ,
+	rateLimitTicker *time.Ticker,
 	rateLimitFlag bool,
+	retries int,
 ) {
 	defer wg.Done()
 
@@ -147,20 +153,37 @@ func worker(
 	}
 
 	for url := range jobsCh {
+		result := checkURL(&httpClient, url, rateLimitTicker, rateLimitFlag, retries)
+		resultsCh <- result
+	}
+}
+
+func checkURL(
+	httpClient *http.Client,
+	url string, 
+	rateLimitTicker *time.Ticker,
+	rateLimitFlag bool, 
+	retries int, 
+) URLStatus {
+	backoff := intialBackoff
+
+	for _ = range retries + 1 {
 		if rateLimitFlag {
 			<- rateLimitTicker.C 
 			// Block until a message is received from this channel 
 			// this helps us achieve rate limiting 
+			// Rate limiting applies per attempt (including retries)
 		}
 
 		startTime := time.Now()
 		resp, err := httpClient.Get(url)
 		responseTime := time.Since(startTime).Milliseconds()
-		if err != nil {
-			resultsCh <- URLStatus{url, 0, 0}
-			continue
+		if err == nil && resp.StatusCode < 500 {
+			defer resp.Body.Close()
+			return URLStatus{url, resp.StatusCode, responseTime}
 		}
-		resultsCh <- URLStatus{url, resp.StatusCode, responseTime}
-		resp.Body.Close()
+		time.Sleep(backoff)
+		backoff *= 2
 	}
+	return URLStatus{url, 0, 0}
 }
